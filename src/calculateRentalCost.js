03bdf7204e4eda8501dc8600ca8996b8a5cbcaed
Yar14k <tabachuk1406@gmail.com>
2025-11-05 17:19:34 +0100

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,11 +9,14 @@ function calculateRentalCost(days) {
   const MEDIUM_TERM_DAYS = 3;
   const LONG_TERM_DISCOUNT = 50;
   const MEDIUM_TERM_DISCOUNT = 20;
+
   const total = days * COST_PER_DAY;
 
   if (days >= LONG_TERM_DAYS) {
     return total - LONG_TERM_DISCOUNT;
-  } else if (days >= MEDIUM_TERM_DAYS) {
+  }
+
+  if (days >= MEDIUM_TERM_DAYS) {
     return total - MEDIUM_TERM_DISCOUNT;
   }
 
